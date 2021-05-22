eefb1bfaf9120cefea4dda31963d92cd0617f7fa
seunghwan <tmdghks3264@naver.com>
2021-05-22 11:19:57 +0900

    Sort - 가장 큰수

--- src/Sort/Solution3.java
+++ src/Sort/Solution3.java
@@ -7,18 +7,15 @@ public class Solution3 {
         int answer = 0;
 
         Arrays.sort(citations);
-        int size = citations.length;
 
-        if(size - size/2 < citations[size/2]){
-            return size - size/2;
-        }else{
-            return citations[size/2];
+        for(int i=0 ; i<citations.length;i++){
+            int a = citations.length -i;
+
+            if(citations[i] >= a){
+                answer = a;
+                break;
+            }
         }
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {20,19,18,1};
-        Solution3 solution3 = new Solution3();
-        System.out.println(solution3.solution3(arr));
+        return answer;
     }
 }
