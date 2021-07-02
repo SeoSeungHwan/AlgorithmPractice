7e32e53e1620603942c81ce3e862b2b0618a121d
DESKTOP-RE5PC2N\seunghwan <tmdghks3264@naver.com>
2021-07-02 23:07:28 +0900

    [2021 KAKAO BLIND RECRUITMENT] 신규 아이디 추천

---
+++ src/Level1/NewId.java
@@ -0,0 +1,50 @@
+package Level1;
+
+public class NewId {
+    public String newid(String new_id) {
+
+        //1단계
+        String answer = new_id.toLowerCase();
+
+        //2단계
+        answer = answer.replaceAll("[~!@#$%^&*()=+\\[{\\]}:?,<>/]","");
+
+        //3단계
+        while(answer.contains("..")){
+            answer = answer.replace("..",".");
+        }
+
+        //4단계
+
+        if(answer.equals(".")){
+            answer="";
+        }
+        if(!answer.equals("") && answer.charAt(0)=='.') {
+            answer = answer.substring(1);
+        }
+
+        if(!answer.equals("") && answer.charAt(answer.length()-1)=='.'){
+            answer = answer.substring(0,answer.length()-1);
+        }
+
+
+        //5단계
+        if(answer.equals("")){
+            answer="a";
+        }
+
+        //6단계
+        if(answer.length()>=16){
+            answer = answer.substring(0,15);
+            if(answer.charAt(answer.length()-1)=='.'){
+                answer = answer.substring(0,14);
+            }
+        }
+
+        //7단계
+        while(answer.length()<=2){
+            answer = answer+answer.charAt(answer.length()-1);
+        }
+        return answer;
+    }
+}
