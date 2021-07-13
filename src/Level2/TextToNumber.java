305c327140e7bf5f950ac2eefd7f7f34b0fde2e9
tmdgh <tmdghks3264@naver.com>
2021-07-13 16:07:08 +0900

    2021 카카오 채용연계형 인턴십 - 숫자 문자열과 영단어

---
+++ src/Level2/TextToNumber.java
@@ -0,0 +1,27 @@
+package Level2;
+
+//https://programmers.co.kr/learn/courses/30/lessons/81301
+//2021 카카오 채용연계형 인턴십 - 숫자 문자열과 영단어
+public class TextToNumber {
+    public int texttonumber(String s) {
+        int answer = 0;
+
+        String[][] str = {{"0","zero"},
+                {"1","one"},
+                {"2","two"},
+                {"3","three"},
+                {"4","four"},
+                {"5","five"},
+                {"6","six"},
+                {"7","seven"},
+                {"8","eight"},
+                {"9","nine"}
+        };
+
+        for(int i=0 ; i<10; i++){
+            s = s.replaceAll(str[i][1],str[i][0]);
+        }
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
