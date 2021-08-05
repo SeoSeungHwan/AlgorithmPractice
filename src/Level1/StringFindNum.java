52e632bffb48998df525736441e1e55e3c5c5383
tmdgh <tmdghks3264@naver.com>
2021-08-05 15:05:01 +0900

    연습문제 - 두정수사이의 합 연습문제 - 나누어 떨어지는 숫자 배열 연습문제 - 같은숫자는 싫어 연습문제 - 숫자찾기 연습문제 - 문자열다루기 연습문제 - 수박수박수박수박수?

---
+++ src/Level1/StringFindNum.java
@@ -0,0 +1,18 @@
+package Level1;
+
+//연습문제 - 숫자찾기
+public class StringFindNum {
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        char[] arr = s.toCharArray();
+        for(int i=0 ; i<arr.length; i++){
+            if('0'<=arr[i] && arr[i]<='9'){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/Level1/StringTest.java
@@ -0,0 +1,23 @@
+package Level1;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12918
+//연습문제 - 문자열다루기
+public class StringTest {
+    public boolean stringtest(String s) {
+        boolean answer = true;
+
+        char[] arr = s.toCharArray();
+
+        if(!(arr.length ==4 || arr.length == 6)){
+            return false;
+        }
+        for(int i=0 ; i<arr.length; i++){
+            if('0'<=arr[i] && arr[i]<='9'){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return answer;
+    }
+}
