06638229d46544bf502a17eb8507e382c5315938
tmdgh <tmdghks3264@naver.com>
2021-08-12 23:08:23 +0900

    연습문제 - 하샤드 수 연습문제 - 직사각형 별찍기 연습문제 - 행렬의 덧셈 연습문제 - 핸드폰 번호 가리기

---
+++ src/Level1/ArrSum.java
@@ -0,0 +1,17 @@
+package Level1;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12950
+//연습문제 - 행렬의 덧셈
+
+public class ArrSum {
+    public int[][] arrsum(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        for(int i=0; i< arr1.length; i++){
+            for(int j=0; j<arr1[i].length; j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
