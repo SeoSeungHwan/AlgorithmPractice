6e0465512755adf1f2c0a77bbcb36ccea9b469a9
DESKTOP-RE5PC2N\seunghwan <tmdghks3264@naver.com>
2021-08-31 22:42:18 +0900

    연습문제 - 문자열 내 마음대로 정렬하기

---
+++ src/Level1/StringSort.java
@@ -0,0 +1,22 @@
+package Level1;
+
+//연습문제 - 문자열 내 마음대로 정렬하기
+//https://programmers.co.kr/learn/courses/30/lessons/12915
+
+import java.util.*;
+public class StringSort {
+    public String[] stringsort(String[] strings, int n) {
+        String[] answer = strings;
+        for(int i=0; i<answer.length; i++){
+            answer[i] = answer[i].charAt(n)+answer[i].substring(0,n)+answer[i].substring(n+1);
+        }
+
+        Arrays.sort(answer);
+        System.out.println(answer[0]);
+        for(int i=0; i<answer.length; i++){
+            answer[i] = answer[i].substring(1,n+1)+answer[i].charAt(0)+answer[i].substring(n+1);
+        }
+
+        return answer;
+    }
+}
