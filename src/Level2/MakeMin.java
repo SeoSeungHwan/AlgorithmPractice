015ae38ad8892f0be34a982a17d3970e745801e4
tmdgh <tmdghks3264@naver.com>
2021-09-28 15:14:43 +0900

    연습문제 - n개의 최소공배수 연습문제 - 최솟값 만들기

---
+++ src/Level2/MakeMin.java
@@ -0,0 +1,24 @@
+package Level2;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12941
+//연습문제 - 최솟값 만들기
+
+
+import java.util.*;
+class MakeMin
+{
+    public int makemin(int []A, int []B)
+    {
+        int answer = 0;
+
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i=0; i<A.length ; i++){
+            answer+= A[i] * B[B.length-1-i];
+        }
+
+        return answer;
+    }
+}
