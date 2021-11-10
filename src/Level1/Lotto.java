46dc4280b9d1f4464a591bb47f2a711f98576576
tmdgh <tmdghks3264@naver.com>
2021-11-10 17:25:18 +0900

    로또의 최고 순위와 최저 순위(다른 방법)

--- src/Level1/Lotto.java
+++ src/Level1/Lotto.java
@@ -1,6 +1,8 @@
 package Level1;
 
-//로또의 최고 순위와 최저 순위
+import java.util.ArrayList;
+
+//로또의 최고 순위와 최저 순위(다른 방법)
 //https://programmers.co.kr/learn/courses/30/lessons/77484
 class Lotto {
     public int[] lotto(int[] lottos, int[] win_nums) {
@@ -36,3 +38,57 @@ class Lotto {
         return answer;
     }
 }
+
+//다른 방법
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+
+        ArrayList<Integer> lottosArrayList = new ArrayList<>();
+        ArrayList<Integer> winArrayList = new ArrayList<>();
+        for(int i=0; i<win_nums.length; i++){
+            winArrayList.add(win_nums[i]);
+        }
+
+        int zero = 0;
+        for(int i=0; i<lottos.length; i++){
+            winArrayList.remove(Integer.valueOf(lottos[i]));
+            if(lottos[i] == 0){
+                zero++;
+            }
+        }
+
+        System.out.println(winArrayList.size());
+
+
+        int min = 6- winArrayList.size();
+        int max = min + zero;
+
+        int[] answer = new int[2];
+        answer[0] = rank(max);
+        answer[1] = rank(min);
+        return answer;
+
+    }
+
+    public int rank(int num){
+        if(num ==6){
+            return 1;
+        }
+        else if(num ==5){
+            return 2;
+        }
+        else if(num ==4){
+            return 3;
+        }
+        else if(num ==3){
+            return 4;
+        }
+        else if(num ==2){
+            return 5;
+        }
+        else{
+            return 6;
+        }
+    }
+}
+
