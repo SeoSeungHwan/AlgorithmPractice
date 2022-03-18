54e589c110b5c3056b5f0639c03ab2187ffb6f8d
tmdghks3264 <tmdghks3264@naver.com>
2022-03-18 16:07:07 +0900

    연습문제 -완전탐색 - 카펫

---
+++ src/Level2/Carpet.java
@@ -0,0 +1,24 @@
+package Level2;
+
+//https://programmers.co.kr/learn/courses/30/lessons/42842
+//연습문제 -완전탐색 - 카펫
+
+class Carpet {
+    public int[] carpet(int brown, int yellow) {
+        int[] answer = new int[2];
+
+        for(int i=yellow ; i>=1; i--){
+            if(yellow%i == 0){
+                int column = (yellow/i) +2;
+                int row = i+2;
+
+                if((column >= row)&& ((column*row)== brown+yellow)){
+                    answer[0] = column;
+                    answer[1] = row;
+
+                }
+            }
+        }
+        return answer;
+    }
+}
