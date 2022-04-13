5d53c069ae19b4a9a7c3643dfd0b40de1cd7b4af
SeoSeungHwan <tmdghks3264@naver.com>
2022-04-13 16:25:41 +0900

    브루트포스(영화감독)

--- src/backjoon/bruteforce/영화감독/Main.java
+++ src/backjoon/bruteforce/영화감독/Main.java
@@ -6,5 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while(count != n) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+
     }
 }
